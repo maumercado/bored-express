b0d755bc98671795e72dcec7a66918521684c76b
Mauricio Mercado <mau@maumercado.com>
2023-04-05 13:26:39 -0700

    Finished level 2 and add README

--- routes/activities.mjs
+++ routes/activities.mjs
@@ -1,6 +1,8 @@
 import express from 'express'
 import axios from 'axios'
+
 import { mapper } from '../lib/mapper.mjs'
+import { getLastUser } from '../lib/users.mjs'
 
 export const BORED_API = 'https://www.boredapi.com/api'
 
@@ -8,9 +10,19 @@ const router = express.Router()
 
 router.get('/activities', async (_, res, next) => {
   try {
+    const lastUser = getLastUser()
+
     const response = await axios.get(`${BORED_API}/activity`)
     const activity = mapper(response.data)
-    return res.json(activity)
+
+    if (
+      lastUser.accessibility === activity.accessibility &&
+      lastUser.price === activity.price
+    ) {
+      return res.json(activity)
+    } else {
+      return res.json({})
+    }
   } catch (error) {
     return next(error)
   }
